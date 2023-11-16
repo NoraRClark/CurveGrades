958b0d3956eb06411023ce7948910cffb27b8093
norac <norac@NCLARK>
2023-11-15 19:46:52 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCurveGrades
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAnalyzeGrades = New System.Windows.Forms.Button()
+        Me.lblNumExams = New System.Windows.Forms.Label()
+        Me.txtNumExams = New System.Windows.Forms.TextBox()
+        Me.txtMean = New System.Windows.Forms.TextBox()
+        Me.lblMean = New System.Windows.Forms.Label()
+        Me.txtStandardDeviation = New System.Windows.Forms.TextBox()
+        Me.lblStandardDeviation = New System.Windows.Forms.Label()
+        Me.dgvDisplayGrades = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvDisplayGrades, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnAnalyzeGrades
+        '
+        Me.btnAnalyzeGrades.Location = New System.Drawing.Point(12, 12)
+        Me.btnAnalyzeGrades.Name = "btnAnalyzeGrades"
+        Me.btnAnalyzeGrades.Size = New System.Drawing.Size(192, 34)
+        Me.btnAnalyzeGrades.TabIndex = 0
+        Me.btnAnalyzeGrades.Text = "Analyze Grades"
+        Me.btnAnalyzeGrades.UseVisualStyleBackColor = True
+        '
+        'lblNumExams
+        '
+        Me.lblNumExams.AutoSize = True
+        Me.lblNumExams.Location = New System.Drawing.Point(20, 60)
+        Me.lblNumExams.Name = "lblNumExams"
+        Me.lblNumExams.Size = New System.Drawing.Size(93, 13)
+        Me.lblNumExams.TabIndex = 1
+        Me.lblNumExams.Text = "Number of Exams:"
+        '
+        'txtNumExams
+        '
+        Me.txtNumExams.Location = New System.Drawing.Point(119, 57)
+        Me.txtNumExams.Name = "txtNumExams"
+        Me.txtNumExams.ReadOnly = True
+        Me.txtNumExams.Size = New System.Drawing.Size(88, 20)
+        Me.txtNumExams.TabIndex = 2
+        '
+        'txtMean
+        '
+        Me.txtMean.Location = New System.Drawing.Point(119, 83)
+        Me.txtMean.Name = "txtMean"
+        Me.txtMean.ReadOnly = True
+        Me.txtMean.Size = New System.Drawing.Size(88, 20)
+        Me.txtMean.TabIndex = 4
+        '
+        'lblMean
+        '
+        Me.lblMean.AutoSize = True
+        Me.lblMean.Location = New System.Drawing.Point(76, 86)
+        Me.lblMean.Name = "lblMean"
+        Me.lblMean.Size = New System.Drawing.Size(37, 13)
+        Me.lblMean.TabIndex = 3
+        Me.lblMean.Text = "Mean:"
+        '
+        'txtStandardDeviation
+        '
+        Me.txtStandardDeviation.Location = New System.Drawing.Point(119, 109)
+        Me.txtStandardDeviation.Name = "txtStandardDeviation"
+        Me.txtStandardDeviation.ReadOnly = True
+        Me.txtStandardDeviation.Size = New System.Drawing.Size(88, 20)
+        Me.txtStandardDeviation.TabIndex = 6
+        '
+        'lblStandardDeviation
+        '
+        Me.lblStandardDeviation.AutoSize = True
+        Me.lblStandardDeviation.Location = New System.Drawing.Point(12, 112)
+        Me.lblStandardDeviation.Name = "lblStandardDeviation"
+        Me.lblStandardDeviation.Size = New System.Drawing.Size(101, 13)
+        Me.lblStandardDeviation.TabIndex = 5
+        Me.lblStandardDeviation.Text = "Standard Deviation:"
+        '
+        'dgvDisplayGrades
+        '
+        Me.dgvDisplayGrades.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvDisplayGrades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDisplayGrades.Location = New System.Drawing.Point(222, 12)
+        Me.dgvDisplayGrades.Name = "dgvDisplayGrades"
+        Me.dgvDisplayGrades.ReadOnly = True
+        Me.dgvDisplayGrades.RowHeadersVisible = False
+        Me.dgvDisplayGrades.Size = New System.Drawing.Size(195, 331)
+        Me.dgvDisplayGrades.TabIndex = 8
+        '
+        'frmCurveGrades
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 349)
+        Me.Controls.Add(Me.dgvDisplayGrades)
+        Me.Controls.Add(Me.txtStandardDeviation)
+        Me.Controls.Add(Me.lblStandardDeviation)
+        Me.Controls.Add(Me.txtMean)
+        Me.Controls.Add(Me.lblMean)
+        Me.Controls.Add(Me.txtNumExams)
+        Me.Controls.Add(Me.lblNumExams)
+        Me.Controls.Add(Me.btnAnalyzeGrades)
+        Me.Name = "frmCurveGrades"
+        Me.Text = "Curve Grades"
+        CType(Me.dgvDisplayGrades, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAnalyzeGrades As Button
+    Friend WithEvents lblNumExams As Label
+    Friend WithEvents txtNumExams As TextBox
+    Friend WithEvents txtMean As TextBox
+    Friend WithEvents lblMean As Label
+    Friend WithEvents txtStandardDeviation As TextBox
+    Friend WithEvents lblStandardDeviation As Label
+    Friend WithEvents dgvDisplayGrades As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,179 @@
+﻿
+
+' This program takes the values in textfile ExamScores.txt and diplays its total number of scores, mean, standard deviation, and a table of
+' each score and the curve letter grade it earns in dgvDisplayGrades
+
+
+Public Class frmCurveGrades
+
+    ' Creates a structure to be used in displaying the numeric score and letter grade in dgvDisplayGrades
+    Structure CurvedScores
+        Dim numberScore As Integer
+        Dim letterGrade As Char
+    End Structure
+
+    Dim earnedGrades(13) As CurvedScores ' Declares an array with the data type of the structure above (CurvedScores)
+
+    ' Sub handles the button clicked event for the form 
+    Private Sub btnAnalyzeGrades_Click(sender As Object, e As EventArgs) Handles btnAnalyzeGrades.Click
+
+        ' Reads data in the txtfile ExamScores.txt and assigns it to a temporary string array (will be copied into a double array)
+        Dim tempExamArray() As String = IO.File.ReadAllLines("ExamScores.txt")
+
+
+        Dim numExams As Integer = tempExamArray.Length ' Finds length of the array (used to determine array sizes and for loop itterations)
+
+        Dim examScores(numExams) As Double ' Declares array with datatype double and same length as tempExamArray()
+
+        ' For loop used to covert and copy data from tempExamArray() to examScores()
+        For i As Integer = 0 To (numExams - 1)
+            examScores(i) = CDbl(tempExamArray(i))
+        Next
+
+        ' Assigns sumExams equal to the value returned from CalculateSum(examScores) where sum of all array element was returned
+        Dim sumExams As Double = CalculateSum(examScores)
+
+        ' Assigns meanExams equal to CalculateMean(numExams, sumExams) where mean of all the exam scores was returned
+        Dim meanExams As Double = CalculateMean(numExams, sumExams)
+
+        ' Assigns standardDeviationExams = CalculateStandardDeviation(examScores, meanExams, numExams) where SD of array values is calculated
+        Dim standardDeviationExams As Double = CalculateStandardDeviation(examScores, meanExams, numExams) ' 
+
+        Dim examLetterGrade() As String ' Creates string array for the letter grades students earned 
+        ' Makes examLetterGrade referece array of curved letter grades earned returned from CreateLetterGradeArray where they were
+        ' calculated using standard deviation 
+        examLetterGrade = CreateLetterGradeArray(examScores, meanExams, standardDeviationExams)
+
+        ' Displays each value into their respective txtboxes and properly formats them
+        txtNumExams.Text = numExams
+        txtMean.Text = meanExams.ToString("N2")
+        txtStandardDeviation.Text = standardDeviationExams.ToString("N2")
+
+        ' Displays contents of DisplayGrades where both arrays are used to display thier respective information in dgvDisplayGrades
+        DisplayGrades(examScores, examLetterGrade, numExams)
+
+    End Sub
+
+
+    ' Fuction CalculateSum uses the array examScores() and adds each value of the array together to find the arrays sum 
+    Function CalculateSum(examScores() As Double) As Double
+
+        Dim sumExams As Double = 0 ' Initalizes var used to return sum outside loop and sets = 0 
+
+        ' For loop goes through each value in array and each itteration adds the value to total 
+        For i As Integer = 0 To (examScores.Count - 1)
+            sumExams += examScores(i)
+        Next
+
+        Return sumExams ' Now contains total sum of array and is returned to btn click sub 
+
+    End Function
+
+    ' Fuction CalculateMean calculates and returns the mean of the array using the sum and size of the array
+    Function CalculateMean(numExams As Double, sumExams As Double) As Double
+
+        Dim meanExams As Double
+        meanExams = sumExams / numExams ' Calculates mean by doing formula (sum of array / # of values in array)
+
+        Return meanExams ' Returns calculated mean to btn click sub
+
+    End Function
+
+    ' Function CalculateStandardDeviation calculated the Standard Deviation of the exam scores in the array 
+    Function CalculateStandardDeviation(examScores() As Double, meanExams As Double, numExams As Integer) As Double
+
+        ' EQUATION FOR STANDARD DEVIATION 
+        ' s = Standard Deviation, m = mean, n = length of array 
+        ' S = √((examScores(0)-m)^2 + (examScores(1)-m)^2...)) / (n)
+
+
+        Dim numeratorSD As Double = 0 ' Initalizes var for the numerator of the SD equation
+        Dim variance As Double ' varience is part of equation inside square root (numeratorSD / n)
+        Dim standardDeviationExams As Double ' SD = square root of the varience
+
+        For i As Integer = 0 To (numExams - 1)
+
+            ' Variable for calculation done to each individual element before being added to total of numerator 
+            Dim singleElementCalc As Double
+            singleElementCalc = Math.Pow(examScores(i) - meanExams, 2)
+
+            numeratorSD += singleElementCalc ' Adds calculated values of each elements together (at end has num total)
+        Next
+
+        variance = numeratorSD / numExams ' Calculates varience by dividing the numerator calculated above by the the length of the array
+
+        standardDeviationExams = Math.Sqrt(variance) ' Finds SD by taking sqare root of the vrience calculated above
+
+
+        Return standardDeviationExams ' Returns the fully calculated SD
+
+    End Function
+
+    ' Function CreateLetterGradeArray uses the array of numeric scores to find curved letter grade each score earned
+    Function CreateLetterGradeArray(numericScores() As Double, meanExams As Double, standardDeviationExams As Double) As String()
+
+
+        Dim numExams As Integer = numericScores.Length ' Calculates size of the arrays
+        Dim examLetterGrade(numExams) As String ' Declares String array for examLetterGrade
+
+
+        ' Declares variables for the upper and lower limits of each curved letter grade 
+        Dim aLowerCuttOff As Double = meanExams + (1.5 * standardDeviationExams)
+
+        Dim bUpperCutoff As Double = meanExams + (1.5 * standardDeviationExams)
+        Dim bLowerCuttOff As Double = meanExams + (0.5 * standardDeviationExams)
+
+        Dim cUpperCutoff As Double = meanExams + (0.5 * standardDeviationExams)
+        Dim cLowerCuttOff As Double = meanExams - (0.5 * standardDeviationExams)
+
+        Dim dUpperCutoff As Double = meanExams - (0.5 * standardDeviationExams)
+        Dim dLowerCuttOff As Double = meanExams - (1.5 * standardDeviationExams)
+
+        Dim fUpperCutoff As Double = meanExams - (1.5 * standardDeviationExams)
+
+        ' Uses for loop, scores of the double array, & limits calculated above to determine grade each score and add letter to the String array
+        For i As Integer = 0 To (numExams - 1)
+
+            If numericScores(i) >= aLowerCuttOff Then
+                examLetterGrade(i) = "A"
+
+            ElseIf numericScores(i) >= bLowerCuttOff And numericScores(i) < bUpperCutoff Then
+                examLetterGrade(i) = "B"
+
+            ElseIf numericScores(i) >= cLowerCuttOff And numericScores(i) < cUpperCutoff Then
+                examLetterGrade(i) = "C"
+
+            ElseIf numericScores(i) >= dLowerCuttOff And numericScores(i) < dUpperCutoff Then
+                examLetterGrade(i) = "D"
+
+            ElseIf numericScores(i) < fUpperCutoff Then
+                examLetterGrade(i) = "F"
+
+            End If
+
+        Next
+
+        Return examLetterGrade ' Returns the filled string array back to the btn clicked event sub 
+
+
+    End Function
+
+    ' Sub DisplayGrades uses Double array of numeric Scores and string array of letter grades to display the info of each into dgvDisplayGrades
+    Sub DisplayGrades(examNumericScores() As Double, examLetterGrade() As String, numExams As Integer)
+
+        ' For loop assigns the values of the two passed array into the array earnedGrades with datatype of Structure CurvedScores (declared
+        ' at top of program) 
+        For i As Integer = 0 To (numExams - 1)
+            earnedGrades(i).numberScore = examNumericScores(i)
+            earnedGrades(i).letterGrade = examLetterGrade(i)
+        Next
+
+        ' Uses structure array earnedGrades in query to retrieve data to be display properly 
+        Dim gradesQuery = From earned In earnedGrades
+                          Select Score = earned.numberScore, Grade = earned.letterGrade
+
+        ' Uses query and DataSource to display information into dgvDisplayGrades
+        dgvDisplayGrades.DataSource = gradesQuery.ToList()
+
+    End Sub
+End Class
